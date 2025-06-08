a6af88f1852eff9e2328f61c29374e86056b8187
Nicole Yu <nicole.yu@kellogg.northwestern.edu>
2025-06-08 23:41:01 +0000

    unwell

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,22 +1,25 @@
 class EntriesController < ApplicationController
-before_action :require_login
+  before_action :require_login
   
   def new
     @user = User.find_by({ "id" => session["user_id"] })
   end
 
   def create
-   @user = User.find_by({ "id" => session["user_id"] })
-  if @user != nil
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry.uploaded_image.attach(params["uploaded_image"])
-    @entry["place_id"] = params["place_id"]
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+    @user = User.find_by({ "id" => session["user_id"] })
+    
+    if @user != nil
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry.uploaded_image.attach(params["uploaded_image"])
+      @entry["place_id"] = params["place_id"]
+      @entry.save
+      redirect_to "/places/#{@entry["place_id"]}"
     else
       flash["notice"] = "Login first."
-  end 
+      redirect_to login_path
+    end
+  end
 end
